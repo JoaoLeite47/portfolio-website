7b92aed2e4b999bfaa5be923478466de6d867385
JoaoLeite47 <StalkerJJ@outlook.com>
2022-07-03 15:28:54 -0300

    feat<topbar> hamburguer menu working

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@ import Topbar from "./components/topbar/Topbar";
 import Work from "./components/work/Work";
 import "./App.css";
 import { useState } from "react";
+import Menu from "./components/menu/Menu";
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div className="app">
       <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <div className="sections">
         <Intro />
         <Portfolio />

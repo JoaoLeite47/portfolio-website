8483021a7276c6a491001263726a0fbd3e248e86
JoaoLeite47 <StalkerJJ@outlook.com>
2022-07-03 12:11:09 -0300

    fix<Testimonials> sintax error

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Contact from "./components/contact/Contact";
+import Intro from "./components/intro/Intro";
+import Portfolio from "./components/portfolio/Portfolio";
+import Testimonials from "./components/Testimonials/Testimonials";
+import Topbar from "./components/topbar/Topbar";
+import Work from "./components/work/Work";
+
+function App() {
+  return (
+    <div className="app">
+      <Topbar />
+      <div className="sections">
+        <Intro />
+        <Portfolio />
+        <Work />
+        <Testimonials />
+        <Contact />
+      </div>
+    </div>
+  );
+}
+
+export default App;

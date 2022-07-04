535122a975449f1c790e77d1a5c7eadbd08aa0a6
JoaoLeite47 <StalkerJJ@outlook.com>
2022-07-04 16:24:43 -0300

    feat<contact> social medias and picture to contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,41 @@
-import React from "react";
+// import { useState } from "react";
 import "./Contact.css";
+import { BsGithub, BsLinkedin, BsWhatsapp } from "react-icons/bs";
 
 export default function Contact() {
-  return <div className="contact" id="contact"></div>;
+  // const [message, setMessage] = useState(false);
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   setMessage(true);
+  // }; // function and hook for submit the mensage to my email inbox
+
+  return (
+    <div className="contact" id="contact">
+      <div className="left">
+        <img src="assets/foto2.jpeg" alt="" />
+      </div>
+      <div className="right">
+        <h2>Contact.</h2>
+        <div className="contacts">
+          <a href="https://github.com/JoaoLeite47">
+            <BsGithub size="5em" />
+          </a>
+          <a href="https://www.linkedin.com/in/joaoleitesouza/">
+            <BsLinkedin size="5em" />
+          </a>
+          <a href="https://api.whatsapp.com/send?phone=5571986149734&text=Olá%20João!!%20Estou%20aqui%20pelo%20link%20em%20seu%20site!">
+            <BsWhatsapp size="5em" />
+          </a>
+          <h3>Thanks! I'll reply soon! :D</h3>
+        </div>
+
+        {/* <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Email" />
+          <textarea placeholder="Message"></textarea>
+          <button type="submit">Send</button>
+          {message && <span> Thanks! I'll reply soon! :D</span>}
+        </form> */}
+      </div>
+    </div>
+  );
 }
